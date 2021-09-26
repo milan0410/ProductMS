f56da0e6eb6bab7346fe977810a05234f2532f44
Milan Vishnoi <milan.vish10@gmail.com>
2021-09-26 18:16:43 +0530

    Updated the packages

---
+++ src/main/java/com/myproject/product/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.myproject.product.repository;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.myproject.product.entity.Product;
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+	    public List<Product> findAllBycategory(String category);
+	    public List<Product> findAllByproductName(String name);
+	    public List<Product> findAllBysellerId(Integer sellerId);
+	}
+
